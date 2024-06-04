8a25bbd5f68539a392695b384789980f354ccd4a
Larissa Fernandes <larissa-cdz@hotmail.com>
2024-06-04 17:23:44 -0300

    homepage: conteudo principal

--- src/views/home-page.view.vue
+++ src/views/home-page.view.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="prose text-justify">
+  <div class="prose">
     <h2>Hello World :)</h2>
     <div class="grid grid-cols-2 gap-8">
       <div class="flex flex-col">
@@ -17,6 +17,32 @@
           Oii! Muito prazer eu sou a <span class="text__highlight">Larissa</span>, uma dev front-end
           :)
         </p>
+        <p class="text-justify">
+          Trabalho profissionalmente com <span class="text__highlight">front-end</span> desde 2021
+          (mas aqui entre nós, o primeiro contato com <span class="text__highlight">HTML</span> e
+          <span class="text__highlight">CSS</span> foi aos meus 13 anos, no
+          <span class="text__highlight">blogger.com</span>).
+        </p>
+      </div>
+      <div class="flex flex-col justify-between">
+        <p class="border border-primary p-2 text-center !my-0" id="about-me__text">
+          Larissa <span class="text-secondary">•</span> 25 anos
+          <span class="text-secondary">•</span> Potiguar
+        </p>
+        <div class="divider divider-secondary w-20 self-center" />
+        <div>
+          <p class="text-justify !my-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut urna ante, pretium ac urna
+            nec, ullamcorper hendrerit tortor. Quisque luctus vulputate justo eu volutpat.
+            Vestibulum lacinia, enim eget eleifend feugiat, sapien nisi porttitor orci, vitae
+            porttitor mi turpis eu elit.
+          </p>
+        </div>
+        <div class="divider divider-secondary w-20 self-center" />
+        <img
+          src="@/assets/home-page/about-me_1.jpg"
+          class="!m-0 aspect-video w-full object-cover"
+        />
       </div>
     </div>
   </div>
